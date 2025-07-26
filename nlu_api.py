f478e3e468a4403ab9c4ab439139c5ca39af18fa
S.M.Shahriar Rahman Maruph <62034743+sm-maruph@users.noreply.github.com>
2025-07-26 21:52:16 +0600

    Update nlu_api.py

--- nlu_api.py
+++ nlu_api.py
@@ -115,4 +115,4 @@ def predict_intent():
         return jsonify({"error": "Internal server error"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5005)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
