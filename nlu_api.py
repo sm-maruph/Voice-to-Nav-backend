2412cb664f73cc73d094349a257eaf7eda80e415
S.M.Shahriar Rahman Maruph <62034743+sm-maruph@users.noreply.github.com>
2025-07-27 01:06:04 +0600

    Update nlu_api_final.py

--- nlu_api.py
+++ nlu_api.py
@@ -74,9 +74,7 @@ intent_map = {
 
 app = Flask(__name__)
 CORS(app)
-@app.route("/", methods=["GET"])
-def index():
-    return jsonify({"status": "OK", "message": "Voice-to-Nav backend is live 🚀"}), 200
+
 # 🔍 Step 4: Predict Intent Endpoint
 @app.route("/predict", methods=["POST"])
 def predict_intent():
